bbe451d7f008524158c84b2e62cf9f4df413bfff
matheushenriquefs <matheus.henrique1021@gmail.com>
2024-03-24 13:22:10 -0300

    feat: fetch forecasts based on coordinates

--- src/components/WeatherWidget/WeatherWidget.ce.vue
+++ src/components/WeatherWidget/WeatherWidget.ce.vue
@@ -1,13 +1,65 @@
 <script setup lang="ts">
 import { ref, onBeforeMount } from "vue";
+import { fetchWeatherApi } from "openmeteo";
 
-import { TheForecast } from "../TheForecast";
-import { DayForecast } from "../DayForecast";
+import { CurrentForecast } from "../CurrentForecast";
+import { DailyForecast } from "../DailyForecast";
+import { useGetCurrentForecastFn } from "../../composables/useGetCurrentForecastFn";
+import { useGetDailyForecastsFn } from "../../composables/useGetDailyForecastsFn";
+import { type WeatherForecast } from "../../types";
 
+const props = withDefaults(
+  defineProps<{
+    latitude?: number;
+    longitude?: number;
+  }>(),
+  {
+    latitude: 51.477928, // prime meridian
+    longitude: -0.001545, // prime meridian
+  },
+);
+
+const weatherForecast = ref<WeatherForecast>({
+  current: null,
+  daily: [],
+});
 const isLoading = ref(true);
 
-onBeforeMount(() => {
-  setTimeout(() => (isLoading.value = false), 1000);
+onBeforeMount(async () => {
+  const params = {
+    // latitude: -23.5475,
+    // longitude: -46.6361,
+    latitude: props.latitude,
+    longitude: props.longitude,
+    current: [
+      "weather_code",
+      "temperature_2m",
+      "apparent_temperature",
+      "relative_humidity_2m",
+      "rain",
+      "wind_speed_10m",
+    ],
+    daily: ["weather_code", "temperature_2m_min", "temperature_2m_max"],
+  };
+  const url = "https://api.open-meteo.com/v1/forecast";
+  const [response] = await fetchWeatherApi(url, params);
+  const current = response.current();
+  const daily = response.daily();
+  const currentForecast = useGetCurrentForecastFn(current, params.current);
+  const dailyForecasts = useGetDailyForecastsFn(
+    daily,
+    {
+      start: Number(daily!.time()) * 1000,
+      end: Number(daily!.timeEnd()) * 1000,
+      interval: daily!.interval() * 1000,
+    },
+    params.daily,
+  );
+  weatherForecast.value = {
+    current: currentForecast,
+    daily: dailyForecasts,
+  };
+  isLoading.value = false;
 });
 </script>
 
@@ -19,10 +71,22 @@ onBeforeMount(() => {
       href="https://fonts.googleapis.com/css2?family=Inter:wght@300..700&display=swap"
       rel="stylesheet"
     />
-    <TheForecast :is-loading class="grid forecast-grid" />
-    <div class="grid day-forecast-container">
-      <div class="day-forecast-grid">
-        <DayForecast v-for="i in 7" :key="i" :is-loading class="column" />
+    <CurrentForecast
+      :current-forecast="
+        weatherForecast.current ? weatherForecast.current : undefined
+      "
+      :is-loading
+      class="grid current-forecast-grid"
+    />
+    <div class="grid daily-forecast-container">
+      <div class="daily-forecast-grid">
+        <DailyForecast
+          v-for="(dailyForecast, i) in weatherForecast.daily"
+          :key="i"
+          :is-loading
+          :daily-forecast
+          class="column"
+        />
       </div>
     </div>
   </div>
@@ -34,8 +98,8 @@ onBeforeMount(() => {
 @import "../../assets/stylesheets/utilities.css";
 @import "../ThePlaceholder/index.css";
 @import "modress/dist/small.css";
-@import "../TheForecast/index.css";
-@import "../DayForecast/index.css";
+@import "../CurrentForecast/index.css";
+@import "../DailyForecast/index.css";
 @import "../TemperatureLabel/index.css";
 @import "../../assets/stylesheets/icons.css";
 
@@ -59,32 +123,11 @@ onBeforeMount(() => {
   padding: var(--spacing-base) var(--spacing-base) 0;
 }
 
-.day-forecast-container {
-  margin: 0 calc(var(--spacing-base) * -1);
-}
-
-.day-forecast-grid {
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: column;
-  gap: var(--spacing-base);
-  width: 100%;
-}
-
 @media (prefers-color-scheme: dark) {
   :host {
     --font-color: var(--neutral-100);
     --body-color: var(--neutral-800);
     --card-body-color: var(--neutral-700);
-  }
-}
-
-@container (min-width: 576px) {
-  .day-forecast-grid {
-    align-items: center;
-    flex-direction: row;
-    gap: unset;
-    margin: 0 var(--spacing-base);
   }
 }
 </style>
