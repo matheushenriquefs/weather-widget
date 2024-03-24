d462a87b004e9fc17f5205a3f89e1aeb1669a18e
matheushenriquefs <matheus.henrique1021@gmail.com>
2024-03-24 12:27:51 -0300

    refactor: return weather label and matching icon, move type declaration to its dedicated file

--- src/composables/useGetWeatherLabelFn/useGetWeatherLabelFn.ts
+++ src/composables/useGetWeatherLabelFn/useGetWeatherLabelFn.ts
@@ -1,35 +1,5 @@
-const codes = {
-  0: "Clear sky",
-  1: "Mainly clear",
-  2: "Partly cloudy",
-  3: "Overcast",
-  45: "Fog",
-  48: "Depositing rime fog",
-  51: "Light drizzle",
-  53: "Moderate drizzle",
-  55: "Heavy drizzle",
-  56: "Light freezing drizzle",
-  57: "Heavy freezing drizzle",
-  61: "Slight rain",
-  63: "Moderate rain",
-  65: "Heavy rain",
-  66: "Slight freezing rain",
-  67: "Heavy freezing rain",
-  71: "Slight snowfall",
-  73: "Moderate snowfall",
-  75: "Heavy snowfall",
-  77: "Snow grains",
-  80: "Slight rain showers",
-  81: "Moderate rain showers",
-  82: "Heavy rain showers",
-  85: "Slight snow showers",
-  86: "Heavy snow showers",
-  95: "Thunderstorm",
-  96: "Thunderstorm with slight and heavy hail",
-  99: "Thunderstorm with slight and heavy hail",
-};
-
-type WeatherCode = keyof typeof codes;
+import { codes } from "../../utils/codes";
+import { type WeatherCode } from "../../types";
 
 export const useGetWeatherLabelFn = (code: WeatherCode) =>
   codes[code] ? codes[code] : codes[0];
---
+++ src/utils/codes.ts
@@ -0,0 +1,129 @@
+import {
+  Sun as SunIcon,
+  SunMedium as SunMediumIcon,
+  CloudSun as CloudSunIcon,
+  Cloudy as CloudyIcon,
+  CloudDrizzle as CloudDrizzleIcon,
+  CloudFog as CloudFogIcon,
+  CloudHail as CloudHailIcon,
+  CloudSnow as CloudSnowIcon,
+  CloudRain as CloudRainIcon,
+  CloudRainWind as CloudRainWindIcon,
+  CloudLightning as CloudLightningIcon,
+  Snowflake as SnowflakeIcon,
+} from "lucide-vue-next";
+
+export const codes = {
+  0: {
+    label: "Clear sky",
+    icon: SunIcon,
+  },
+  1: {
+    label: "Mainly clear",
+    icon: SunMediumIcon,
+  },
+  2: {
+    label: "Partly cloudy",
+    icon: CloudSunIcon,
+  },
+  3: {
+    label: "Overcast",
+    icon: CloudyIcon,
+  },
+  45: {
+    label: "Fog",
+    icon: CloudFogIcon,
+  },
+  48: {
+    label: "Depositing rime fog",
+    icon: CloudFogIcon,
+  },
+  51: {
+    label: "Light drizzle",
+    icon: CloudDrizzleIcon,
+  },
+  53: {
+    label: "Moderate drizzle",
+    icon: CloudDrizzleIcon,
+  },
+  55: {
+    label: "Heavy drizzle",
+    icon: CloudDrizzleIcon,
+  },
+  56: {
+    label: "Light freezing drizzle",
+    icon: CloudSnowIcon,
+  },
+  57: {
+    label: "Heavy freezing drizzle",
+    icon: CloudHailIcon,
+  },
+  61: {
+    label: "Slight rain",
+    icon: CloudDrizzleIcon,
+  },
+  63: {
+    label: "Moderate rain",
+    icon: CloudRainIcon,
+  },
+  65: {
+    label: "Heavy rain",
+    icon: CloudRainWindIcon,
+  },
+  66: {
+    label: "Slight freezing rain",
+    icon: CloudHailIcon,
+  },
+  67: {
+    label: "Heavy freezing rain",
+    icon: CloudHailIcon,
+  },
+  71: {
+    label: "Slight snowfall",
+    icon: SnowflakeIcon,
+  },
+  73: {
+    label: "Moderate snowfall",
+    icon: SnowflakeIcon,
+  },
+  75: {
+    label: "Heavy snowfall",
+    icon: SnowflakeIcon,
+  },
+  77: {
+    label: "Snow grains",
+    icon: SnowflakeIcon,
+  },
+  80: {
+    label: "Slight rain showers",
+    icon: CloudDrizzleIcon,
+  },
+  81: {
+    label: "Moderate rain showers",
+    icon: CloudRainIcon,
+  },
+  82: {
+    label: "Heavy rain showers",
+    icon: CloudRainWindIcon,
+  },
+  85: {
+    label: "Slight snow showers",
+    icon: SnowflakeIcon,
+  },
+  86: {
+    label: "Heavy snow showers",
+    icon: SnowflakeIcon,
+  },
+  95: {
+    label: "Thunderstorm",
+    icon: CloudLightningIcon,
+  },
+  96: {
+    label: "Thunderstorm with slight and heavy hail",
+    icon: CloudLightningIcon,
+  },
+  99: {
+    label: "Thunderstorm with slight and heavy hail",
+    icon: CloudLightningIcon,
+  },
+};
