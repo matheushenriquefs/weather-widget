819940959d27a758ce2ea443e8ec21d37d8befb6
matheushenriquefs <matheus.henrique1021@gmail.com>
2024-03-24 16:52:13 -0300

    build: set options for lib mode and display lib bundle stats

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 /// <reference types="vitest" />
+import { resolve } from "path";
+
 import { defineConfig } from "vite";
 import { configDefaults } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +16,9 @@ export default defineConfig({
         },
       },
     }),
+    visualizer({
+      gzipSize: true,
+    }),
   ],
   server: {
     host: true,
@@ -21,6 +27,13 @@ export default defineConfig({
   preview: {
     host: true,
     port: 3333,
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "WeatherWidget",
+      fileName: "weather-widget",
+    },
   },
   test: {
     globals: true,
