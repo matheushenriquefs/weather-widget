15ee8feb1a5ee0ca134c3f488157051e4c70a6da
matheushenriquefs <matheus.henrique1021@gmail.com>
2024-04-06 21:48:37 -0300

    refactor: add fallback values if there are missing options

--- src/composables/useGetCurrentForecastFn/useGetCurrentForecastFn.ts
+++ src/composables/useGetCurrentForecastFn/useGetCurrentForecastFn.ts
@@ -7,24 +7,26 @@ export const useGetCurrentForecastFn = (
   current: VariablesWithTime | null,
   options: string[],
 ): CurrentForecast => {
+  const defaultForecast = {
+    date: Date.now(),
+    temperature: {
+      min: 0,
+      max: 0,
+      apparent: 0,
+    },
+    weather: {
+      code: 0 as WeatherCode,
+      label: useGetWeatherLabelFn(0).label,
+    },
+    wind: {
+      speed: 0,
+    },
+    relativeHumidity: 0,
+    rain: 0,
+  };
+
   if (!current) {
-    return {
-      date: Date.now(),
-      temperature: {
-        min: 0,
-        max: 0,
-        apparent: 0,
-      },
-      weather: {
-        code: 0,
-        label: useGetWeatherLabelFn(0).label,
-      },
-      wind: {
-        speed: 0,
-      },
-      relativeHumidity: 0,
-      rain: 0,
-    };
+    return defaultForecast;
   }
 
   const weatherCodeIndex = options.indexOf("weather_code");
@@ -33,28 +35,48 @@ export const useGetCurrentForecastFn = (
   const relativeHumidityIndex = options.indexOf("relative_humidity_2m");
   const rainIndex = options.indexOf("rain");
   const windSpeedIndex = options.indexOf("wind_speed_10m");
+
   const date = Number(current.time()) * 1000;
-  const code = current.variables(weatherCodeIndex)!.value() as WeatherCode;
+
+  const code = current.variables(weatherCodeIndex)
+    ? (current.variables(weatherCodeIndex)!.value() as WeatherCode)
+    : defaultForecast.weather.code;
+
+  const temperature = current.variables(temperatureIndex)
+    ? current.variables(temperatureIndex)!.value()
+    : defaultForecast.temperature.min;
+
+  const apparentTemperature = current.variables(apparentTemperatureIndex)
+    ? current.variables(apparentTemperatureIndex)!.value()
+    : defaultForecast.temperature.apparent;
+
+  const relativeHumidity = current.variables(relativeHumidityIndex)
+    ? current.variables(relativeHumidityIndex)!.value()
+    : defaultForecast.relativeHumidity;
+
+  const rain = current.variables(rainIndex)
+    ? current.variables(rainIndex)!.value()
+    : defaultForecast.rain;
+
+  const windSpeed = current.variables(windSpeedIndex)
+    ? current.variables(windSpeedIndex)!.value()
+    : defaultForecast.wind.speed;
 
   return {
     date,
     temperature: {
-      min: Math.round(current.variables(temperatureIndex)!.value()),
-      max: Math.round(current.variables(temperatureIndex)!.value()),
-      apparent: Math.round(
-        current.variables(apparentTemperatureIndex)!.value(),
-      ),
+      min: Math.round(temperature),
+      max: Math.round(temperature),
+      apparent: Math.round(apparentTemperature),
     },
     weather: {
       code,
       label: useGetWeatherLabelFn(code).label,
     },
     wind: {
-      speed: Math.round(current.variables(windSpeedIndex)!.value()),
+      speed: Math.round(windSpeed),
     },
-    relativeHumidity: Math.round(
-      current.variables(relativeHumidityIndex)!.value(),
-    ),
-    rain: Math.round(current.variables(rainIndex)!.value()),
+    relativeHumidity: Math.round(relativeHumidity),
+    rain: Math.round(rain),
   };
 };
--- src/composables/useGetDailyForecastsFn/useGetDailyForecastsFn.ts
+++ src/composables/useGetDailyForecastsFn/useGetDailyForecastsFn.ts
@@ -13,6 +13,15 @@ export const useGetDailyForecastsFn = (
   options: string[],
 ) => {
   const dailyForecasts: DailyForecast[] = [];
+  const defaultForecast = {
+    temperature: {
+      min: 0,
+      max: 0,
+    },
+    weather: {
+      code: 0 as WeatherCode,
+    },
+  };
 
   if (!daily) {
     return dailyForecasts;
@@ -25,19 +34,42 @@ export const useGetDailyForecastsFn = (
     const weatherCodeIndex = options.indexOf("weather_code");
     const minTemperatureIndex = options.indexOf("temperature_2m_min");
     const maxTemperatureIndex = options.indexOf("temperature_2m_max");
-    const code = daily.variables(weatherCodeIndex)!.valuesArray()![
-      index
-    ] as WeatherCode;
+    let code = defaultForecast.weather.code;
+    let minTemperature = defaultForecast.temperature.min;
+    let maxTemperature = defaultForecast.temperature.max;
+
+    if (
+      daily.variables(weatherCodeIndex)?.valuesArray() &&
+      daily.variables(weatherCodeIndex)!.valuesArray()![index] !== undefined
+    ) {
+      code = daily.variables(weatherCodeIndex)!.valuesArray()![
+        index
+      ] as WeatherCode;
+    }
+
+    if (
+      daily.variables(minTemperatureIndex)?.valuesArray() &&
+      daily.variables(minTemperatureIndex)!.valuesArray()![index] !== undefined
+    ) {
+      minTemperature = daily.variables(minTemperatureIndex)!.valuesArray()![
+        index
+      ];
+    }
+
+    if (
+      daily.variables(maxTemperatureIndex)?.valuesArray() &&
+      daily.variables(maxTemperatureIndex)!.valuesArray()![index] !== undefined
+    ) {
+      maxTemperature = daily.variables(maxTemperatureIndex)!.valuesArray()![
+        index
+      ];
+    }
 
     dailyForecasts.push({
       date: nextDate,
       temperature: {
-        min: Math.round(
-          daily.variables(minTemperatureIndex)!.valuesArray()![index],
-        ),
-        max: Math.round(
-          daily.variables(maxTemperatureIndex)!.valuesArray()![index],
-        ),
+        min: Math.round(minTemperature),
+        max: Math.round(maxTemperature),
       },
       weather: {
         code,
